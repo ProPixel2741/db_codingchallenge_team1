c1fcc8bd910afac33eb7c1cfac0c1040363b9db4
BrionGahl <BrionGahl@gmail.com>
2023-08-04 15:53:43 -0400

    login state.

--- java-api/src/main/java/com/db/grad/javaapi/controller/UsersController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/UsersController.java
@@ -14,12 +14,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 @RequestMapping("/api/v1/auth")
 public class UsersController {
 
