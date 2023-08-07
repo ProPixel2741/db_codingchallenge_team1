43bf986a0d6d322b1a01cb303ecbe9a44ae66158
BrionGahl <BrionGahl@gmail.com>
2023-08-07 10:08:32 -0400

    user based filtering

--- react-app/src/components/dashboard/Dashboard.jsx
+++ react-app/src/components/dashboard/Dashboard.jsx
@@ -1,26 +1,72 @@
 import React, { useState, useEffect } from "react";
 import { DataGrid } from '@mui/x-data-grid';
-import { Button } from "@mui/material";
-import { useLocation } from "react-router-dom";
-import { getAllTrades } from "../../services/DashboardService";
+import { Button, Stack, Checkbox, Divider } from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getAllTrades, getUserTrades } from "../../services/DashboardService";
 
 export const Dashboard = () => {
     const { state } = useLocation();
+    const navigate = useNavigate();
+
     const [data, setData] = useState([]);
+
+    const [myBondFilter, setMyBondFilter] = useState(false);
+    const [myBondData, setMyBondData] = useState([]);
+    const [bondData, setBondData] = useState([]);
+
+    const cols = [
+        {field: "id", headerName: "ID", width: 50},
+        {field: "currency", headerName: "Currency", width: 100},
+        {field: "quantity", headerName: "Quantity", width: 100},
+        {field: "unit_price", headerName: "Unit Price", width: 100},
+        {field: "buy_sell", headerName: "Buy / Sell", width: 100},
+        {field: "book_name", headerName: "Trading Book", width: 100},
+        {field: "counter_party_name", headerName: "Counter Party", width: 100},
+        {field: "isin", headerName: "ISIN", width: 100},
+        {field: "cusip", headerName: "CUSIP", width: 100},
+        {field: "issuer_name", headerName: "Bond Issuer", width: 100},
+        {field: "maturity_date", headerName: "Maturity Date YYYY-MM-DD", width: 100},
+        {field: "coupon", headerName: "Coupon%", width: 100},
+        {field: "type", headerName: "Bond Type", width: 100},
+        {field: "face_value", headerName: "Face Value", width: 100},
+        {field: "security_currency", headerName: "Security Currency", width: 100},
+        {field: "status", headerName: "Status", width: 100},
+        {field: "trade_date", headerName: "Trade Date YYYY-MM-DD", width: 100},
+        {field: "settlement_date", headerName: "Settlement Date YYYY-MM-DD", width: 100},
+    ];
+
+    const rows = [];
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        window.location.href="http://localhost:3000/";
-    }
+        navigate('/', {state: {}});
+    };
+
+    const handleMyBondsFilterToggle = () => {
+        setMyBondFilter(!myBondFilter)
+        console.log(myBondFilter)
+        return
+    };
 
     useEffect(() => {
         getAllTrades(state.token)
-              .then(({data}) => {
-              setData(data);
-              });
+            .then(({data}) => {
+                setData(data);
+                setBondData(data)
+            });
+        getUserTrades(state.token, state.username)
+            .then(({data}) => {
+                setMyBondData(data);
+            });
     }, [state]);
     
-    const rows = [];
+    useEffect(() => {
+        if (myBondFilter) {
+            setData(myBondData);
+        } else {
+            setData(bondData);
+        }
+    }, [myBondFilter]);
 
     function jsonParseToRow(jsonObject, uniqueId) {
         return {
@@ -49,32 +95,17 @@ export const Dashboard = () => {
         rows.push(jsonParseToRow(data[i], i+1));
     }
 
-    const cols = [
-        {field: "id", headerName: "ID", width: 150},
-        {field: "currency", headerName: "Currency", width: 150},
-        {field: "quantity", headerName: "Quantity", width: 150},
-        {field: "unit_price", headerName: "Unit Price", width: 150},
-        {field: "buy_sell", headerName: "Buy / Sell", width: 150},
-        {field: "book_name", headerName: "Trading Book", width: 150},
-        {field: "counter_party_name", headerName: "Counter Party", width: 150},
-        {field: "isin", headerName: "ISIN", width: 150},
-        {field: "cusip", headerName: "CUSIP", width: 150},
-        {field: "issuer_name", headerName: "Bond Issuer", width: 150},
-        {field: "maturity_date", headerName: "Maturity Date YYYY-MM-DD", width: 150},
-        {field: "coupon", headerName: "Coupon%", width: 150},
-        {field: "type", headerName: "Bond Type", width: 150},
-        {field: "face_value", headerName: "Face Value", width: 150},
-        {field: "security_currency", headerName: "Security Currency", width: 150},
-        {field: "status", headerName: "Status", width: 150},
-        {field: "trade_date", headerName: "Trade Date YYYY-MM-DD", width: 150},
-        {field: "settlement_date", headerName: "Settlement Date YYYY-MM-DD", width: 150},
-    ];
-
-    
-
     return (
         <>
         <h1>Bond Dashboard</h1>
+        <div className={"filters"}>
+            <Stack label="Filters" spacing={2} direction="row" divider={<Divider orientation="vertical" flexItem/>}>
+                <div>
+                    <label>Show my Bonds</label>
+                    <Checkbox checked={myBondFilter} label="Show my Bonds" onChange={handleMyBondsFilterToggle}/>  
+                </div>
+            </Stack>
+        </div>
         <div>
             <DataGrid rows={rows} columns={cols} getRowId={(row) => row.id}
                 sx={{
@@ -88,11 +119,6 @@ export const Dashboard = () => {
                 }}
             />
             <Button variant="contained" onClick={handleSubmit}>Logout</Button>
-        </div>
-        <div>
-            {/* Display tradesData and booksData in your component */}
-            <h2>Trades Data:</h2>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
         </>
     )
